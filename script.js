970d9a45498c4e013547bf379a60e2edcb5a8f1d
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-01 18:58:50 +0800

    change of plans

--- script.js
+++
@@ -1,8 +0,0 @@
-let mitskiaudio = document.querySelector(".audiomitski");
-
-const album1 = document.querySelector(".mitski").addEventListener("click", () => {
-    mitskiaudio.setAttribute("autoplay");
-});
-const album2 = document.querySelector(".tyler");
-const album3 = document.querySelector(".mcr");
-const album4 = document.querySelector(".beyonce");
