6f77c048520147f0ff7927ff4911d2946fd671e2
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-01 20:17:00 +0800

    update js

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ mitskiIMG.addEventListener("mouseout", () => {
 });
 const mitskiAudio = document.querySelector("#mitskiaudio");
 
+const tylerIMG = document.querySelector(".tyler");
+tylerIMG.addEventListener("click", () => {
+    tylerAudio.play();
+})
+tylerIMG.addEventListener("mouseout", () => {
+    tylerAudio.pause();
+})
+const tylerAudio = document.querySelector("#tyleraudio");
+
+const mcrIMG = document.querySelector(".mcr");
+mcrIMG.addEventListener("click", () => {
+    mcrAudio.play();
+})
+mcrIMG.addEventListener("mouseout", () => {
+    mcrAudio.pause();
+})
+const mcrAudio = document.querySelector("#mcraudio");
+
+const beyonceIMG = document.querySelector(".beyonce");
+beyonceIMG.addEventListener("click", () => {
+    beyonceAudio.play();
+})
+beyonceIMG.addEventListener("mouseout", () => {
+    beyonceAudio.pause();
+})
+const beyonceAudio = document.querySelector("#beyonceaudio");
