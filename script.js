165aff160c14e9c2e680ccd5e4c84fa6f9504ae3
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-01 20:07:51 +0800

    fixed audio

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// AUDIOS
+const mitskiIMG = document.querySelector(".mitski");
+mitskiIMG.addEventListener("click", () => {
+    mitskiAudio.play()
+});
+mitskiIMG.addEventListener("mouseout", () => {
+    mitskiAudio.pause()
+});
 const mitskiAudio = document.querySelector("#mitskiaudio");
-const tylerAudio = document.querySelector("#tyleraudio");
-const mcrAudio = document.querySelector("#mcraudio");
-const beyonceAudio = document.querySelector("#beyonceaudio");
 
-// ALBUM PHOTOS
-const mitskiIMG = document.querySelector(".mitski").addEventListener("mouseover", () => {hoverImg(mitskiAudio)});
-const tylerIMG = document.querySelector(".tyler").addEventListener("mouseover", () => {hoverImg(tylerAudio)});
-const mcrIMG = document.querySelector(".mcr").addEventListener("mouseover", () => {hoverImg(mcrAudio)});
-const beyonceIMG = document.querySelector(".beyonce").addEventListener("mouseover",() => {hoverImg(beyonceAudio)});
-
-function hoverImg(audio) {
-    if ("mouseover" === true) {
-        audio.play();
-    }
-    else if ("mouseout" === true) {
-        audio.pause();
-    }
-}
