98ac5c65b99309edca47acecf3b75561f2495dd9
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-01 18:34:32 +0800

    trying to make audio work

---
+++ script.js
@@ -0,0 +1,8 @@
+let mitskiaudio = document.querySelector(".audiomitski");
+
+const album1 = document.querySelector(".mitski").addEventListener("click", () => {
+    mitskiaudio.setAttribute("autoplay");
+});
+const album2 = document.querySelector(".tyler");
+const album3 = document.querySelector(".mcr");
+const album4 = document.querySelector(".beyonce");
